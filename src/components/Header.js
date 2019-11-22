9e74b45fb163b21375727bcda3bd71fe6ee216e2
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-21 17:08:37 -0800

    started with state as Loading True

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.css';
 
 const Header = () => (
-  <header>
+  <header className={styles.Header}>
     <nav>
       <Link to="/memes"> My Memes </Link>
       <Link to="/"> Create Meme </Link>
